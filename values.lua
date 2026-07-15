76ff34ecfed28632553427c5e2bd6a3c7f19ee9c
nosniy-games <110691200+nosniy-games@users.noreply.github.com>
2026-07-14 22:04:24 -0400

    Add values.lua with item values

---
+++ values.lua
@@ -0,0 +1,13 @@
+return {
+	["Ice Serpent"]=30,
+	["Black Dragon"]=30,
+	["Sun Bloom"]=8,
+	["Star Fruit"]=1.5,
+	["Raccoon"]=0.75,
+	["Super Watering Can"]=0.006,
+	["Super Sprinkler"]=0.003,
+	["Dragon's Breath"]=0.08,
+	["Moon Bloom"]=0.015,
+	["Hypno Bloom"]=0.02,
+	["Mega"]=0.0008,
+}
