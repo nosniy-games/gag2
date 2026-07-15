f95776c5babc9ae2fe17e988cc31c4b380f9c159
nosniy-games <110691200+nosniy-games@users.noreply.github.com>
2026-07-14 22:18:34 -0400

    Update values.lua

--- values.lua
+++ values.lua
@@ -4,8 +4,11 @@ return {
 	["Sun Bloom"]=8,
 	["Star Fruit"]=1.5,
 	["Raccoon"]=0.75,
+	["Super Watering Can"]=0.006,
+	["Super Sprinkler"]=0.003,
 	["Dragon's Breath"]=0.08,
 	["Moon Bloom"]=0.015,
 	["Hypno Bloom"]=0.02,
+	["Common Egg"]=0.01,
 	["Mega"]=0.0008,
 }
